c9e7a3a514a12b30bd570239729e8eee3e99abb2
AngerOverApathy <violasian@gmail.com>
2023-04-15 18:52:50 -0700

    functionality added

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`)
     if (!audio) return;
+    audio.currentTime = 0 //rewind to start
     audio.play()
 })
