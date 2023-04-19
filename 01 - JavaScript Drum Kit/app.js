b0e89188e362320aa15c460599e6277460487807
AngerOverApathy <violasian@gmail.com>
2023-04-18 19:09:17 -0700

    key styling added

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,16 +1,17 @@
-document.addEventListener('keydown', function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`) //find audio element
     const key = document.querySelector(`.key[data-key='${e.keyCode}']`) //find key class and data-key
     if (!audio) return;
     audio.currentTime = 0 //rewind to start
     audio.play()
     key.classList.add('playing') //add css class to key
-})
+}
 
 function removeTransition(e){
     if(e.propertyName !== 'transform') return; //skip if not a transform
     this.classList.remove('playing')
 }
 
+document.addEventListener('keydown', playSound)
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
